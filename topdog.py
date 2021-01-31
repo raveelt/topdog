6f4080a55ce2ea43ff4bc6d6de2d8b0bf0d45607
raveelt <64331687+raveelt@users.noreply.github.com>
2021-01-31 14:06:45 -0800

    Class Update
    Accidentally duplicated projectile class, deleted unnnessesary code

--- topdog.py
+++ topdog.py
@@ -147,15 +147,6 @@ class enemyplane(object):
        
         pygame.draw.rect(win, (0,0,0), (1000, 10 , 250, 10), 2)
 
-class projectile(object):
-    def __init__(self, x, y, radius, color):
-        self.x = x
-        self.y = y
-        self.radius = radius
-        self. color = color
-
-    def draw(self, win):
-        pygame.draw.circle(win, self.color, (int(self.x), int(self.y)), self.radius)
 
 
 
